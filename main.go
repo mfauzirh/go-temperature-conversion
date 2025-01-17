0a075cb954b7186fa132e166044534ecaed4c062
mfauzirh <mfauzirizkih@gmail.com>
2025-01-17 09:13:48 +0700

    refactor converter to implement struct and interface

--- main.go
+++ main.go
@@ -2,128 +2,112 @@ package main
 
 import "fmt"
 
+type Celsius struct {
+	Temperature float64
+}
+
+func (c *Celsius) ToCelsius() float64 {
+	return c.Temperature
+}
+
+func (c *Celsius) ToFahrenheit() float64 {
+	return ((9.0 / 5.0) * c.Temperature) + 32.0
+}
+
+func (c *Celsius) ToKelvin() float64 {
+	return c.Temperature + 273.15
+}
+
+type Fahrenheit struct {
+	Temperature float64
+}
+
+func (f *Fahrenheit) ToCelsius() float64 {
+	return (f.Temperature - 32.0) * (5.0 / 9.0)
+}
+
+func (f *Fahrenheit) ToFahrenheit() float64 {
+	return f.Temperature
+}
+
+func (f *Fahrenheit) ToKelvin() float64 {
+	return (f.Temperature + 459.67) * (5.0 / 9.0)
+}
+
+type Kelvin struct {
+	Temperature float64
+}
+
+func (k *Kelvin) ToCelsius() float64 {
+	return k.Temperature - 273.15
+}
+
+func (k *Kelvin) ToFahrenheit() float64 {
+	return (k.Temperature * (9.0 / 5.0)) - 459.67
+}
+
+func (k *Kelvin) ToKelvin() float64 {
+	return k.Temperature
+}
+
+type TemperatureConverter interface {
+	ToCelsius() float64
+	ToFahrenheit() float64
+	ToKelvin() float64
+}
+
 func main() {
-	fmt.Println("Enter desired temperature conversion")
-	fmt.Println("1. Celsius to Fahrenheit")
-	fmt.Println("2. Celsius to Kelvin")
-	fmt.Println("3. Fahrenheit to Celsius")
-	fmt.Println("4. Fahrenheit to Kelvin")
-	fmt.Println("5. Kelvin to Celsius")
-	fmt.Println("6. Kelvin to Fahrenheit")
+	fmt.Println("Enter your current temperature")
+	fmt.Println("1. Celsius")
+	fmt.Println("2. Fahrenheit")
+	fmt.Println("3. Kelvin")
 	fmt.Print("Enter your choice: ")
 
-	var choice int
-	fmt.Scanf("%d", &choice)
-	for choice < 1 || choice > 6 {
-		fmt.Print("Temperature conversion not available. Enter your choice: ")
-		fmt.Scanf("%d", &choice)
+	var fromTemperature int
+	fmt.Scanf("%d", &fromTemperature)
+	for fromTemperature < 1 || fromTemperature > 6 {
+		fmt.Print("Choice is not available. Enter your choice: ")
+		fmt.Scanf("%d", &fromTemperature)
+	}
+
+	fmt.Println("Enter your desired temperature conversion to")
+	fmt.Println("1. Celsius")
+	fmt.Println("2. Fahrenheit")
+	fmt.Println("3. Kelvin")
+	fmt.Print("Enter your choice: ")
+
+	var toTemperature int
+	fmt.Scanf("%d", &toTemperature)
+	for toTemperature < 1 || toTemperature > 6 {
+		fmt.Print("Choice is not available. Enter your choice: ")
+		fmt.Scanf("%d", &toTemperature)
 	}
 
 	var temperature float64
 	fmt.Print("\nEnter temperature: ")
 	fmt.Scanf("%f", temperature)
 
-	var finalTemperature float64
-	switch choice {
+	var temperatureConverter TemperatureConverter
+	switch fromTemperature {
 	case 1:
-		finalTemperature = CelsiusToFahrenheit(temperature)
+		temperatureConverter = &Celsius{temperature}
 	case 2:
-		finalTemperature = CelsiusToKelvin(temperature)
+		temperatureConverter = &Fahrenheit{temperature}
 	case 3:
-		finalTemperature = FahrenheitToCelsius(temperature)
-	case 4:
-		finalTemperature = FahrenheitToKelvin(temperature)
-	case 5:
-		finalTemperature = KelvinToCelsius(temperature)
-	case 6:
-		finalTemperature = KelvinToFahrenheit(temperature)
+		temperatureConverter = &Kelvin{temperature}
+	}
+
+	var finalTemperature float64
+	switch toTemperature {
+	case 1:
+		finalTemperature = temperatureConverter.ToCelsius()
+	case 2:
+		finalTemperature = temperatureConverter.ToFahrenheit()
+	case 3:
+		finalTemperature = temperatureConverter.ToKelvin()
 	}
 
 	fmt.Printf("\nFinal temperature after conversion is: %.2f\n", finalTemperature)
 
 	fmt.Println("\nThanks for your time :)")
-}
-
-// Convert temperature from Celsius to Fahrenheit.
-//
-// Parameters:
-//
-// - temperatureCelsius: temperature in Celsius.
-//
-// Returns:
-//
-// - The temperature converted to Fahrenheit.
-func CelsiusToFahrenheit(temperatureCelsius float64) float64 {
-	temperatureFahrenheit := ((9.0 / 5.0) * temperatureCelsius) + 32.0
-	return temperatureFahrenheit
-}
-
-// Convert temperature from Celsius to Kelvin.
-//
-// Parameters:
-//
-// - temperatureCelsius: temperature in Celsius.
-//
-// Returns:
-//
-// - the temperature converted to Kelvin.
-func CelsiusToKelvin(temperatureCelsius float64) float64 {
-	tKelvin := temperatureCelsius + 273.15
-	return tKelvin
-}
-
-// Convert temperature from Fahrenheit to Celsius.
-//
-// Parameters:
-//
-// - temperatureFahrenheit: temperature in Fahrenheit.
-//
-// Returns:
-//
-// - the temperature converted to Celsius.
-func FahrenheitToCelsius(temperatureFahrenheit float64) float64 {
-	temperatureCelsius := (temperatureFahrenheit - 32.0) * (5.0 / 9.0)
-	return temperatureCelsius
-}
-
-// Convert temperature from Fahrenheit to Kelvin.
-//
-// Parameters:
-//
-// - temperatureFahrenheit: temperature in Fahrenheit.
-//
-// Returns:
-//
-// - the temperature converted to Kelvin.
-func FahrenheitToKelvin(temperatureFahrenheit float64) float64 {
-	temperatureKelvin := (temperatureFahrenheit + 459.67) * (5.0 / 9.0)
-	return temperatureKelvin
-}
-
-// Convert temperature from Kelvin to Celsius.
-//
-// Parameters:
-//
-// - temperatureKelvin: temperature in Kelvin.
-//
-// Returns:
-//
-// - the temperature converted to Celsius.
-func KelvinToCelsius(temperatureKelvin float64) float64 {
-	temperatureCelsius := temperatureKelvin - 273.15
-	return temperatureCelsius
-}
-
-// Convert temperature from Kelvin to Fahrenheit.
-//
-// Parameters:
-//
-// - temperatureKelvin: temperature in Kelvin.
-//
-// Returns:
-//
-// - the temperature converted to Fahrenheit.
-func KelvinToFahrenheit(temperatureKelvin float64) float64 {
-	temperatureFahrenheit := (temperatureKelvin * (9.0 / 5.0)) - 459.67
-	return temperatureFahrenheit
 }
